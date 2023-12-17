7e79fca285b07763e4a0983cc2501f38eedc0095
Fernando Santos <nandof3020@gmail.com>
2023-12-17 08:37:42 -0300

    desafio felipao

---
+++ main.js
@@ -0,0 +1,34 @@
+(function(){
+
+let nickName = 'Nando3020'
+let xp = 5001;
+
+
+if(xp <= 1000){
+      console.log('O Heroi de nome '+nickName+' de nivel Ferro');
+}
+ else if(xp <= 2000){
+    console.log('O Heroi de nome '+nickName+' de nivel Bronze');
+
+ }else if(xp <= 5000){
+    console.log('O Heroi de nome '+nickName+' de nivel Prata');
+
+ }else if(xp <= 6000){
+    console.log('O Heroi de nome '+nickName+' de nivel Ouro');
+
+ }else if(xp <= 7000){
+    console.log('O Heroi de nome '+nickName+' de nivel Platina');
+
+ }else if(xp <= 8000){
+    console.log('O Heroi de nome '+nickName+' de nivel Imortal');
+
+ }else if(xp <= 9000){
+    console.log('O Heroi de nome '+nickName+' de nivel Radiante');
+ }
+ else{
+    console.log('  não tem xp  ');
+ }
+
+
+
+}())
